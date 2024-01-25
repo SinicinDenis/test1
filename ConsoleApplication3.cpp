9cffcbf1feb46a31b9d6837c1aba18f7ef0405b6
User <User@K9>
2024-01-25 16:28:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, summ=0, k; cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        summ ^= k;
+    }
+    cout << summ;
+}
